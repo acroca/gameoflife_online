7eaab99de57822475fc52ed537ae633252f051b9
Albert Callarisa <shark234@gmail.com>
2010-10-22 17:54:16 +0200

    Fixed cell click event

--- application.js
+++ application.js
@@ -6,9 +6,11 @@ $(document).ready(function(){
         table.append(row);
         for(var j = 0; j<20; j++){
             var cell = $('<td class="cell row_'+i+' col_'+j+' cell_'+i+'_'+j+'">')
-            cell.click(function(){
-                $.getScript("/add.js?row="+i+"&col="+j);
+            var cell_link = $("<a href='/add.js?row="+i+"&col="+j+"'>")
+            cell_link.click(function(link){
+                $.getScript(link.href);
             });
+            cell.append(cell_link);
             row.append(cell);
         }
     }
