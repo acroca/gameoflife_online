fb5f347ec294368a18f2d66cae8f976efefbffbf
Albert Callarisa <shark234@gmail.com>
2010-10-24 11:12:54 +0200

    Added game logic

--- application.js
+++ application.js
@@ -5,19 +5,43 @@ $(document).ready(function(){
         var row = $("<tr>");
         table.append(row);
         for(var j = 0; j<20; j++){
-            var cell = $('<td class="cell row_'+i+' col_'+j+' cell_'+i+'_'+j+'">')
+            var cell = $('<td id="cell_'+i+'_'+j+'" class="cell row_'+i+' col_'+j+'">')
             var cell_link = $("<a href='/add.js?row="+i+"&col="+j+"'>")
-            cell_link.click(function(link){
-                $.getScript(link.href);
+            cell_link.click(function(event){
+                $.ajax({
+                    url: this.href,
+                    dataType: "script"
+                });
+                event.preventDefault();
             });
             cell.append(cell_link);
             row.append(cell);
         }
     }
 
-    $("#update").click(function(){
-        $.getScript("/update.js");
+    $("#update").click(function(event){
+        update();
+        event.preventDefault();
+    });
+    $("#step").click(function(event){
+        $.ajax({
+            url: "/step.js",
+            dataType: "script"
+        });
+        event.preventDefault();
     });
 
-    
+    var update = function(){
+        $.getJSON("/update.js", function(cells){
+            $(".cell").removeClass("marked");
+            $(cells).each(function(){
+                $("#cell_"+this.row+"_"+this.col).addClass("marked");
+            });
+        });
+    }
+    var updateTO = function(){
+        update();
+        setTimeout(updateTO, 200);
+    };
+    updateTO();
 });
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http'),
     fs = require('fs'),
     url = require('url'),
     querystring = require('querystring'),
+    sys = require("sys"),
 
     mongodb_path = "./node-mongodb-native/lib/mongodb",
     Db = require(mongodb_path).Db,
@@ -24,50 +25,44 @@ send404 = function(res){
     res.end();
 };
 
-var db = new Db('game-of-life-online', 
-                new Server(settings.mongodb.host, settings.mongodb.port, {}), {native_parser:true});
 
-// db.open(function(err, db) {
-//     db.dropDatabase(function(err, result) {
-//         db.collection('test', function(err, collection) {
-//             // Erase all records from the collection, if any
-//             collection.remove(function(err, collection) {
-//                 // Insert 3 records
-//                 for(var i = 0; i < 3; i++) {
-//                     collection.insert({'a':i});
-//                 }
-//             });
-//         });
-//     });
-// });
+var db = new Db('game-of-life-online', new Server(settings.mongodb.host, settings.mongodb.port, {}), {native_parser:true})
+db.open(function(e, db) {}) 
+
+
+db.dropDatabase(function() {});
 
 http.createServer(function (req, res) {
     var path = url.parse(req.url).pathname;
-    console.info("Request received: "+req.url);
-
+    // console.info("Request received: "+req.url);
 
     switch (path){
     case '/update.js':
         update_game(req,res);
+        break;
+    case '/step.js':
+        step();
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+	res.write("", 'utf8');
+	res.end();
         break;
     case '/add.js':
         add_cell(req,res);
         break;
 
 
+        
     case '/':
-        path = "/index.html"
     case "/index.html":
+        staticHandler(req,res, "/index.html", "text/html");
+        break;
     case "/application.js":
     case "/jquery-1.4.3.min.js":
+        staticHandler(req,res, path, "text/javascript");
+        break;
     case "/style.css":
-	fs.readFile(__dirname + path, function(err, data){
-	    if (err) return send404(res);
-            res.writeHead(200, {'Content-Type': 'text/html'});
-	    res.write(data, 'utf8');
-	    res.end();
-	});
-	break;
+        staticHandler(req,res, path, "text/css");
+        break;
     default: send404(res);
     }
 }).listen(settings.port, "127.0.0.1");
@@ -75,16 +70,85 @@ http.createServer(function (req, res) {
 console.log('Server running at http://127.0.0.1:'+settings.port+'/');
 
 
-var update_game = function(req, res){
-    
-}
-var add_cell = function(req, res){
+//-----------------------------------------------------------------------------------
 
-    // TODO: I always get 20,20. It's a problem in application.js.
-    params = url.parse(req.url, true).query;
-    db.open(function(err, db) {
-        db.collection('cells', function(err, collection) {
-            collection.insert({'row': params.row, 'col': params.col});
+var staticHandler = function(req,res, path, content_type){
+    fs.readFile(__dirname + path, function(err, data){
+	if (err) return send404(res);
+        res.writeHead(200, {'Content-Type': content_type});
+	res.write(data, 'utf8');
+	res.end();
+    });
+}
+
+var update_game = function(req, res){
+    db.collection('cells', function(err, collection) {
+        collection.find(function(err, cursor) {
+            cursor.toArray(function(err, cells) {
+                res.writeHead(200, {'Content-Type': 'text/javascript'});
+                res.write(JSON.stringify(cells), 'utf8');
+                res.end();
+            });
         });
     });
 }
+
+var add_cell = function(req, res){
+
+    params = url.parse(req.url, true).query;
+    
+    db.collection('cells', function(err, collection) {
+        collection.insert({'row': params.row, 'col': params.col});
+    });
+    res.writeHead(200, {'Content-Type': 'text/javascript'});
+    res.write("", 'utf8');
+    res.end();
+}
+
+var step = function(){
+
+    board = new Array(20);
+    for(i=0;i<20;i++) {
+        board[i] = new Array(20);
+    }
+    
+    var neighbours = function(board, x, y){
+        var from_x = (x==0 ? 0 : x-1);
+        var to_x   = (x==19 ? 19 : x+1);
+        var from_y = (y==0 ? 0 : y-1);
+        var to_y   = (y==19 ? 19 : y+1);
+        var n      = 0
+
+        for(var i = from_x;i <= to_x;i++){
+            for(var j = from_y;j <= to_y;j++){
+                if(!(i == x && j == y))
+                    if(board[i][j] == true)
+                        n++;
+            }
+        }
+        return n;
+    }
+    db.collection('cells', function(err, collection) {
+        collection.find(function(err, cursor) {
+            cursor.toArray(function(err, cells) {
+                cells.forEach(function(cell) {
+                    board[parseInt(cell.row)][parseInt(cell.col)] = true
+                });
+                collection.remove(function(err, collection) {
+                    for(var i=0;i<20;i++) {
+                        for(var j=0;j<20;j++) {
+                            n = neighbours(board, i, j);
+
+                            if(n==3 || (n==2 && board[i][j]== true))
+                                collection.insert({'row': i, 'col': j});
+                        }
+                    }
+                });
+            });
+        });
+    });
+    db.collection('cells', function(err, collection) {
+    });
+
+}
+
